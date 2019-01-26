75ccea29a5723c3eff2056bc543abfd5c84ec4eb
MalikGallow <gallowabdul@gmail.com>
2019-01-26 15:07:23 +0200

    fixed carousel

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ $('.number').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 4000,
+        duration: 9000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
     });
 });
+
 
