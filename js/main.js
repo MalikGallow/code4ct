73f0a5d922bc7ba323991b0a2d490472aaad66e2
MalikGallow <gallowabdul@gmail.com>
2019-01-28 16:12:31 +0200

    added back to top button

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $('.number').each(function () {
     });
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the page, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -23,10 +23,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top of the page
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 } 
 
 // cart
